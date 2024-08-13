c17f7622238f52a76f8b3d83b17ab22c639ebb24
luizzamprogno <lpzampronio@gmail.com>
2024-08-13 09:30:13 -0300

    Cores adicionadas para as classes de obesidade

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ def calculate_imc(weight, height):
 def get_imc_category(imc):
 
     if imc is None:
-        return ''
+        return '', 'black'
     if imc <= 17:
-       return 'Muito abaixo do peso'
+       return 'Muito abaixo do peso', 'blue'
     elif imc > 17 and imc <= 18.5:
-        return 'Abaixo do peso'
+        return 'Abaixo do peso', 'orange'
     elif imc >18.5 and imc <= 24.9:
-        return 'Peso normal'
+        return 'Peso normal', 'green'
     elif imc >24.9 and imc <= 29.9:
-        return 'Acima do peso'
+        return 'Acima do peso', 'yellow'
     elif imc >29.9 and imc <= 34.9:
-        return 'Obesidade I'
+        return 'Obesidade I', 'red'
     elif imc >34.9 and imc <= 39.9:
-        return 'Obesidade II (severa)'
+        return 'Obesidade II (severa)', 'red'
     else:
-        return 'Obesidade III (mórbida)'
+        return 'Obesidade III (mórbida)', 'red'
 
 def main():
 
@@ -34,15 +34,15 @@ def main():
 
     # Layout
     layout = [
-        [sg.Text('Calcule o seu IMC')],
-        [sg.Text('Seu peso (kg): '), sg.Input(key='peso')],
-        [sg.Text('Altura (m): '), sg.Input(key='altura')],
-        [sg.Button(button_text='CALCULAR')],
+        [sg.Text('Calcule o seu IMC', font=('Arial', 12, 'bold'))],
+        [sg.Text('Seu peso (kg): ', size=(12,1)), sg.Input(key='peso')],
+        [sg.Text('Altura (m): ', size=(12,1)), sg.Input(key='altura')],
+        [sg.Button(button_text='CALCULAR', font=('Arial', 12, 'bold'))],
         [sg.Text('', key='resultado')],
         [sg.Text('', key='resposta')]
     ]
     # Janela
-    window = sg.Window('Calculadora de IMC', layout=layout)
+    window = sg.Window('Calculadora de IMC', layout=layout, element_justification='c')
 
     # Leitura de eventos e valores
     while True:
@@ -56,7 +56,8 @@ def main():
             imc = calculate_imc(peso, altura)
             if imc is not None:
                 window['resultado'].update(f'Seu IMC: {imc}')
-                window['resposta'].update(get_imc_category(imc))
+                category, color = get_imc_category(imc)
+                window['resposta'].update(category, text_color=color)
 
 if __name__ == '__main__':
     main()
